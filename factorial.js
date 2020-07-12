7310ccde55cf9b9f8e54961b43f9a13b08625815
Jessica <reynolds.jessann@gmail.com>
2020-07-12 13:14:35 -0400

    Adds recursiveSumAll

--- factorial.js
+++ factorial.js
@@ -1,12 +1,14 @@
 /**
 Implement a recursive factorial() function which will take a number and compute its factorial value. For example, factorial(5) would return 120 because 5 * 4 * 3 * 2* 1 is 120.
  */
-function factorial (factorial) {
-    return '';
+function recursiveSumAll (num) {
+    if (num === 1) return 1;
+    return num * recursiveSumAll(num - 1);
+
 }
 
-const result1 = factorial(5)
-
+const result1 = recursiveSumAll(5)
+console.log(recursiveSumAll(5))
 /*
  Learn something new!
 
